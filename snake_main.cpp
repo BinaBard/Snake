84cd6733054a28d1f205241c4e4aea79fb079987
BinaBard <kolpakidi2023@mail.ru>
2024-04-30 21:56:34 +0500

    First add

---
+++ snake_main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int fieldwidth, fieldheight;
+	
+	cout <<"Say width";
+	cin >> fieldwidth;
+	cout<< "Say height";
+	cin >> fieldheight;
+	
+	return 0;
+}
